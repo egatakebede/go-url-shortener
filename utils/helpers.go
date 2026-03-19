20795bdf3989a1a35b3a6aea4f40dce5ef3a6690
egatakebede <egatakebede7@gmail.com>
2026-03-19 11:11:06 +0300

    utils

--- utils/helpers.go
+++ utils/helpers.go
@@ -5,12 +5,17 @@ import (
 	"time"
 )
 
+// charset defines characters we use for short codes
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+// GenerateShortCode creates a random string of given length
 func GenerateShortCode(length int) string {
-	rand.Seed(time.Now().UnixNano())
+	// Create a local random generator seeded with current time
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	code := make([]byte, length)
 	for i := range code {
-		code[i] = charset[rand.Intn(len(charset))]
+		code[i] = charset[r.Intn(len(charset))]
 	}
+	return string(code)
 }
